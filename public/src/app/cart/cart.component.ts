b0c7e29fde6cbdb927da88ce452c2e07d8b9fd1b
Brandonv93 <bvicc93@gmail.com>
2021-08-12 17:59:01 -0500

    cart component
    finished edit order section
    finished reviewOrder section

--- public/src/app/cart/cart.component.ts
+++ public/src/app/cart/cart.component.ts
@@ -60,7 +60,82 @@ export class CartComponent implements OnInit {
   ]
 
 
-  onRemove(id:number){
-    this.cart.map()
+  onRemove(itemId:number){
+    for(var i=0; i< this.cart.length; i++){
+      if(this.cart[i].id === itemId){
+        if(this.cart[i].quantity <= 1){
+          let temp = this.cart[i];
+          let itemToBeDeleted = this.cart[i];
+          let lastItem = this.cart[this.cart.length-1];
+          this.cart[i] = lastItem;
+          this.cart[this.cart.length-1] = temp;
+          this.cart.pop()
+          if(itemToBeDeleted.id == 4 || itemToBeDeleted.id == 5){
+            this.addExtras.push(itemToBeDeleted)
+          }else{
+            break
+          }
+        } else{
+          this.cart[i].quantity -= 1
+        }
+      }
+    }
   }
+
+  onAdd(itemId:number){
+    for(var i=0; i< this.cart.length; i++){
+      if(this.cart[i].id === itemId){
+        this.cart[i].quantity += 1;
+      }
+    }
+  }
+
+
+  onAddExtra(itemId:number){
+    for(var i=0; i< this.addExtras.length; i++){
+        if(this.addExtras[i].id === itemId){
+          this.cart.push(this.addExtras[i])
+
+          let temp = this.addExtras[i];
+          let itemToBeDeleted = this.addExtras[i];
+          let lastItem = this.addExtras[this.addExtras.length-1];
+          this.addExtras[i] = lastItem;
+          this.addExtras[this.addExtras.length-1] = temp;
+          this.addExtras.pop()
+          break
+      }
+    }
+  }
+
+
+  cartSubtotal():number{
+    var total:number = 0;
+    for(var i=0; i < this.cart.length; i++){
+      let itemtotal = this.cart[i].quantity * this.cart[i].price; 
+      total += itemtotal;
+    }
+    return total;
+  }
+
+  tax(total:number):string{
+    let num = total * .0825;
+    return num.toFixed(2);
+  }
+
+  tip(total:number):string{
+    let num = total * .1;
+    return num.toFixed(2);
+  }
+
+  cartTotal(num1:any,num2:any,num3:any):string{
+    num2 = parseFloat(num2)
+    num3 = parseFloat(num3)
+    console.log(num1,num2,num3)
+
+    let total = num1+num2+num3;
+    return total;
+  }
+
+
+
 }
